1bd240caf61680153f00701d39fbd8bbb4d00274
dannylin0711 <dannylin0711@gmail.com>
2023-06-25 18:43:32 +0800

    Test using chmod

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+chmod 777 /root/.pkg-cache/
 echo "Packing binaries"
-npx pkg ./build-env -t node16.15.0-linux-arm64 -o ./build/asphyxia-core --options no-warnings
+npx pkg ./build-env -t node16-linux-arm64 -o ./build/asphyxia-core --options no-warnings
