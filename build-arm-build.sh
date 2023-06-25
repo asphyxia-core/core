452c13eced4281bdc0db5886460eb3011da5398c
dannylin0711 <dannylin0711@gmail.com>
2023-06-25 16:18:01 +0800

    Make pkg-fetch get linuxstatic version

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Packing binaries"
-npx pkg ./build-env -t node16-linux-armv7 -o ./build/asphyxia-core --options no-warnings
+npx pkg ./build-env -t node16-linuxstatic-armv7 -o ./build/asphyxia-core --options no-warnings
 # npx pkg ./build-env -t node16-linux-arm64 -o ./build/asphyxia-core --options no-warnings
