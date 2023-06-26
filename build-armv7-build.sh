36c9d8309fa5a62cc5bf734e8f3c0a7ad403324d
dannylin0711 <dannylin0711@gmail.com>
2023-06-27 04:17:53 +0800

    Using my server for prebuilt serving

--- build-armv7-build.sh
+++ build-armv7-build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# apt-get install -y build-essential patch
+apt-get install -y build-essential patch
 
 mkdir /root/.pkg-cache
 mkdir /root/.pkg-cache/node
 chmod 777 /root
 chmod 777 /root/.pkg-cache
 chmod 777 /root/.pkg-cache/node
-export PKG_IGNORE_TAG=true # prevents pkg-fetch to add a tag folder
-curl https://github.com/yao-pkg/pkg-binaries/releases/download/node16/built-v16.16.0-linux-armv7 -o /root/.pkg-cache/built-v16.16.0-linux-armv7 # download the binary, be sure it is prefixewd with built-, otherwise it will not work
+
+curl https://latowolf.freeddns.org/ubuntu/www/built-v16.16.0-linux-armv7 -o /root/.pkg-cache/built-v16.16.0-linux-armv7
 
 echo "Packing binaries"
 npx pkg ./build-env -t node16-linux-armv7 -o ./build/asphyxia-core --options no-warnings
