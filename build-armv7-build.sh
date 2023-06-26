3f732e0b0f0bde3412aa57968b7c76467598c3be
dannylin0711 <dannylin0711@gmail.com>
2023-06-27 04:10:54 +0800

    Use another prebuild

--- build-armv7-build.sh
+++ build-armv7-build.sh
@@ -7,8 +7,8 @@ mkdir /root/.pkg-cache/node
 chmod 777 /root
 chmod 777 /root/.pkg-cache
 chmod 777 /root/.pkg-cache/node
-
-curl https://latowolf.freeddns.org/ubuntu/www/node-v16.19.1-linux-armv7 -o /root/.pkg-cache/node/node-v16.19.1-linux-armv7
+export PKG_IGNORE_TAG=true # prevents pkg-fetch to add a tag folder
+curl https://github.com/yao-pkg/pkg-binaries/releases/download/node16/built-v16.16.0-linux-armv7 -LO /root/.pkg-cache/built-v16.16.0-linux-armv7 # download the binary, be sure it is prefixewd with built-, otherwise it will not work
 
 echo "Packing binaries"
 npx pkg ./build-env -t node16-linux-armv7 -o ./build/asphyxia-core --options no-warnings
