3213148c1a73d64639530628dec63f4b20d8cd66
dannylin0711 <dannylin0711@gmail.com>
2023-06-27 04:21:12 +0800

    Add back deleted lines

--- build-armv7-build.sh
+++ build-armv7-build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-apt-get install -y build-essential patch
-
 mkdir /root/.pkg-cache
 mkdir /root/.pkg-cache/node
 chmod 777 /root
 chmod 777 /root/.pkg-cache
 chmod 777 /root/.pkg-cache/node
 
+export PKG_IGNORE_TAG=true # prevents pkg-fetch to add a tag folder
 curl https://latowolf.freeddns.org/ubuntu/www/built-v16.16.0-linux-armv7 -o /root/.pkg-cache/built-v16.16.0-linux-armv7
 
 echo "Packing binaries"
