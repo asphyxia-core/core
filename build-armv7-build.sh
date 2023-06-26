c48f3c6db26a5953dbcb0e1da3c7740b5319d8b5
dannylin0711 <dannylin0711@gmail.com>
2023-06-26 23:53:29 +0800

    Add back armv7 support

---
+++ build-armv7-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir /root
+mkdir /root/.pkg-cache
+mkdir /root/.pkg-cache/node
+chmod 777 /root
+chmod 777 /root/.pkg-cache
+chmod 777 /root/.pkg-cache/node
+echo "Packing binaries"
+npx pkg ./build-env -t node16-linuxstatic-armv7 -o ./build/asphyxia-core --options no-warnings
