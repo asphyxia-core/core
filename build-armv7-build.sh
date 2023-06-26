f353a465bdfb49ad3bf226a7e2da022e8b775fa3
dannylin0711 <dannylin0711@gmail.com>
2023-06-27 03:39:46 +0800

    Wrong url

--- build-armv7-build.sh
+++ build-armv7-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y build-essential patch
+# apt-get install -y build-essential patch
 
 mkdir /root/.pkg-cache
 mkdir /root/.pkg-cache/node
@@ -8,7 +8,7 @@ chmod 777 /root
 chmod 777 /root/.pkg-cache
 chmod 777 /root/.pkg-cache/node
 
-curl https://latowolf.freeddns.org/ubuntu/node-v16.19.1-linux-armv7 -o /root/.pkg-cache/node/node-v16.19.1-linux-armv7
+curl https://latowolf.freeddns.org/ubuntu/www/node-v16.19.1-linux-armv7 -o /root/.pkg-cache/node/node-v16.19.1-linux-armv7
 
 echo "Packing binaries"
 npx pkg ./build-env -t node16-linux-armv7 -o ./build/asphyxia-core --options no-warnings
