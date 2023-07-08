fb1db1f3743ed5da859cf75b7b184f54ed1750a5
Freddie <whatis@tsdo.in>
2023-07-09 04:48:43 +0800

    do a cleanup

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -7,9 +7,9 @@ chmod 777 /root/.pkg-cache
 chmod 777 /root/.pkg-cache/node
 
 export PKG_IGNORE_TAG=true # prevents pkg-fetch to add a tag folder
-cp ./build-env/pkg-cache/built-v16.16.0-linux-armv7 /root/.pkg-cache/built-v16.16.0-linux-armv7
-chmod 777 /root/.pkg-cache/built-v16.16.0-linux-armv7
 
-echo "Packing binaries"
+echo "Packing armv7"
 npx pkg ./build-env -t node16.16.0-linux-armv7 -o ./build/asphyxia-core --options no-warnings
+
+echo "Packing arm64"
 npx pkg ./build-env -t node16.16.0-linux-arm64 -o ./build/asphyxia-core --options no-warnings
