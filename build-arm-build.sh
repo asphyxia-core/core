f86cb96de943a2a7ab91143b8300e97bec7d6d28
Freddie <whatis@tsdo.in>
2023-07-09 21:20:37 +0800

    armv7 attempt 2

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir /root/.pkg-cache
-mkdir /root/.pkg-cache/node
 chmod 777 /root
+mkdir /root/.pkg-cache
 chmod 777 /root/.pkg-cache
-chmod 777 /root/.pkg-cache/node
 
 export PKG_IGNORE_TAG=true # prevents pkg-fetch to add a tag folder
+cp ./build-env/pkg-cache/built-v16.16.0-linux-armv7 /root/.pkg-cache/built-v16.16.0-linux-armv7
+chmod 777 /root/.pkg-cache/built-v16.16.0-linux-armv7
 
 echo "Packing armv7"
 npx pkg ./build-env -t node16.16.0-linux-armv7 -o ./build/asphyxia-core --options no-warnings
